07f505467531ebcc6552252f7c710aa5254b6f61
Harsh Shukla <108127262+harshshukla2807@users.noreply.github.com>
2024-03-13 19:02:08 +0530

    about fix

--- components/About.jsx
+++ components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <div className="flex-col gap-2">
         <h3 className="text-[12px] font-semibold">What is Bitcoin?</h3>
         <p className="text-[10px]">
-          Bitcoin's price today is US$16,951.82, with a 24-hour trading volume
+          Bitcoin price today is US$16,951.82, with a 24-hour trading volume
           of $19.14 B. BTC is +0.36% in the last 24 hours. It is currently
           -7.70% from its 7-day all-time high of $18,366.66, and 3.40% from its
           7-day all-time low of $16,394.75. BTC has a circulating supply of
