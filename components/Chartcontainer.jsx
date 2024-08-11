8d63c73543d45e92e8e936f9486a1bf1a24e5364
Harsh Shukla <108127262+harshshukla2807@users.noreply.github.com>
2024-08-11 05:36:45 +0530

    upgrade version for fix

--- components/Chartcontainer.jsx
+++ components/Chartcontainer.jsx
@@ -7,7 +7,7 @@ const getdata = async (params) => {
   // console.log(params.slug)
   const queryParams = new URLSearchParams({
     vs_currency: "usd",
-    ids:`${params?.slug}`
+    ids: `${params?.slug}`,
   });
 
   const url = `${rootUrl}/coins/markets?${queryParams}`;
@@ -18,64 +18,76 @@ const getdata = async (params) => {
   return data;
 };
 
-
-const Chartcontainer = async ({params}) => {
+const Chartcontainer = async ({ params }) => {
   // console.log(params)
   const data = await getdata(params);
 
   return (
     <div className="bg-white p-6 rounded-md">
-    {data.map((data,index)=><div key={index}>
-      <div className="flex">
-        <div className="flex gap-3 items-center">
-          <span className="">
-        {console.log(data.image)}
-            <Image
-              loader={()=>data.image}
-              src={data.image}
-              width={32}
-              height={32}
-              alt="bitcoin icon"
-            />
-          </span>
-          <h1 className="text-lg font-semibold">{data.name}</h1>
-          <strong className="text-[#696969] text-sm">{data.symbol}</strong>
-        </div>
-        <button className="bg-[#768396] rounded-md p-3 pb-1 pt-1 ml-6 text-white">
-          Rank #{data.market_cap_rank}
-        </button>
-      </div>
-
-      {/* chart */}
-      <div>
-        <div className="flex-col">
-          <div className="flex mt-5 gap-4 md:gap-10 items-center">
-            <h1 className="font-semibold text-2xl sm:text-4xl">${data.current_price}</h1>
-            <div className={`flex ${data.price_change_percentage_24h>=0? 'bg-[#ecffec]':'bg-red-100'} gap-2 p-1 pl-2 pr-2 rounded-sm`}>
-              <Image
-              className=""
-                src={`/${data.price_change_percentage_24h>=0? 'polygon.svg':'polygon1.svg'}`}
-                height={15}
-                width={15}
-                alt="polygon up image"
-              />
-              <span>{data.price_change_percentage_24h}%</span>
+      {data.map((data, index) => (
+        <div key={index}>
+          <div className="flex">
+            <div className="flex gap-3 items-center">
+              <span className="">
+                <Image
+                  src={data.image}
+                  width={32}
+                  height={32}
+                  alt="bitcoin icon"
+                />
+              </span>
+              <h1 className="text-lg font-semibold">{data.name}</h1>
+              <strong className="text-[#696969] text-sm">{data.symbol}</strong>
             </div>
-            <span>(24H)</span>
+            <button className="bg-[#768396] rounded-md p-3 pb-1 pt-1 ml-6 text-white">
+              Rank #{data.market_cap_rank}
+            </button>
           </div>
-          <h1 className="mt-2 font-medium text-sm sm:text-lg">₹{data.current_price * 82.7}</h1>
+
+          {/* chart */}
+          <div>
+            <div className="flex-col">
+              <div className="flex mt-5 gap-4 md:gap-10 items-center">
+                <h1 className="font-semibold text-2xl sm:text-4xl">
+                  ${data.current_price}
+                </h1>
+                <div
+                  className={`flex ${
+                    data.price_change_percentage_24h >= 0
+                      ? "bg-[#ecffec]"
+                      : "bg-red-100"
+                  } gap-2 p-1 pl-2 pr-2 rounded-sm`}
+                >
+                  <Image
+                    className=""
+                    src={`/${
+                      data.price_change_percentage_24h >= 0
+                        ? "polygon.svg"
+                        : "polygon1.svg"
+                    }`}
+                    height={15}
+                    width={15}
+                    alt="polygon up image"
+                  />
+                  <span>{data.price_change_percentage_24h}%</span>
+                </div>
+                <span>(24H)</span>
+              </div>
+              <h1 className="mt-2 font-medium text-sm sm:text-lg">
+                ₹{data.current_price * 82.7}
+              </h1>
+            </div>
+
+            <hr className="mb-8 mt-8" />
+
+            <div>
+              <h2 className="font-semibold">{data.name} Price Chart (USD)</h2>
+              {/* fetch chart */}
+              <TradingViewWidget />
+            </div>
+          </div>
         </div>
-        
-        <hr className="mb-8 mt-8" />
-      
-        <div>
-            <h2 className="font-semibold">{data.name} Price Chart (USD)</h2>
-            {/* fetch chart */}
-            <TradingViewWidget />
-            
-        </div>
-      </div>
-      </div>)}
+      ))}
     </div>
   );
 };
--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'assets.coingecko.com',
+        hostname: 'coin-images.coingecko.com',
         pathname: '**',
       },
     ],
